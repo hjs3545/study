02cc033a27c0de28e0bb908db79f1f86b81e69f1
hjs3545 <hjs3545@gmail.com>
2022-09-01 00:55:06 +0900

    React

---
+++ React/project1/src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  // className을 이용해서 컴포넌트 스타일링 해줌 이게 정석
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
---
+++ React/project1/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+// import './App.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById('root')); 
+
+//react18부터 지원 안됨
